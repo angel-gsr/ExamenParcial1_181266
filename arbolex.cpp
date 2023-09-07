74c992042889a1e20eedcb4868270a71eb9f2bb6
angel-gsr <141948536+angel-gsr@users.noreply.github.com>
2023-09-07 09:33:14 -0600

    Updated arbolex.cpp

--- arbolex.cpp
+++ arbolex.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdio.h>
 
+//Código del Arbol de Navidad
+
 int main()
 {
 	int i,k,j,c;
@@ -18,35 +20,87 @@ int main()
 			case 'a':
 				printf("Escribe num de pisos para el arbol: \n");
 				scanf("%d",&j);
-				
-				for(i=0;i<j;i++)
+				//primero el arbol
+				i = 0;
+				while (i < j) 
 				{
-					for(k=0;k<6-i;k++)
+				    k = 0;
+				    while (k < j - 1 - i) 
 					{
-						printf(" ");
-					}
-					for(k=0;k<i*2+1;k++)
+				        printf(" ");
+				        k++;
+				    }
+				
+				    k = 0;
+				    while (k < i * 2 + 1) 
 					{
-						printf("*");	
-					}
-					printf("\n");
+				        printf("*");
+				        k++;
+				    }
+				
+				    printf("\n");
+				    i++;
 				}
+				//for (i = 0; i < j - 1; i++) 
+				i=0;
+			 	while(i<j-1)
+				{
+			    // espacios para centrarlo
+			    //for (k = 0; k < j - 1; k++) 
+			    	k=0;
+			    	while(k<j-1)
+					{
+			            printf(" ");
+			            k++;
+			        }
+
+			        // tallo
+			        printf("| |\n");
+			    	i++;
+			 	}
 				break;
 				
 			case 'b':
 				printf("Escribe num de pisos para el arbol: \n");
 				scanf("%d",&j);
-				
-				for (i=j-1;i>=0;i--) {
-			        for (k=0;k<j-i;k++) 
+				//primero el tallo
+				//for (i = 0; i < j - 1; i++) 
+				i=0;
+				while(i<j-1)
+				 {
+			        // espacios para centrarlo
+			        //for (k = 0; k < j - 1; k++) 
+			        k=0;
+			        while(k<j-1)
 					{
 			            printf(" ");
+			            k++;
 			        }
-			        for (k=0;k<2*i+1;k++) 
+
+			        //tallo
+			        printf("| |\n");
+			        i++;
+			    }
+				//for (i=j-1;i>=0;i--) 
+				i=j-1;
+				while(i>=0)
+				{
+			        //for (k=0;k<j-i;k++) 
+			        k=0;
+			        while(k<j-i)
+					{
+			            printf(" ");
+			            k++;
+			        }
+			        //for (k=0;k<2*i+1;k++) 
+			        k=0;
+			        while(k<2*i+1)
 					{
 			            printf("*");
+			            k++;
 			        }
 			        printf("\n");
+			        i--;
 			    }
 			    break;
 			default:
